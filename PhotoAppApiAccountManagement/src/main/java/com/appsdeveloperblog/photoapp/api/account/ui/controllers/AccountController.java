5d8d1106b84fa73d140b13b4525578cd97517b71
MarcosLorencini <mlorencinif@gmail.com>
2023-03-08 10:31:51 -0300

    ms AccountManagement created

--- PhotoAppApiAccountManagement/src/main/java/com/appsdeveloperblog/photoapp/api/account/ui/controllers/AccountController.java
+++ PhotoAppApiAccountManagement/src/main/java/com/appsdeveloperblog/photoapp/api/account/ui/controllers/AccountController.java
@@ -1,17 +1,15 @@
-package com.appsdeveloperblog.api.users.ui.contollers;
+package com.appsdeveloperblog.photoapp.api.account.ui.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
+@RequestMapping("/account")
+public class AccountController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working...";
     }
-
-
 }
